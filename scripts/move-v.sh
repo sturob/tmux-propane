41245e4f327f67605aa0041fcb26c62368668bc4
Stu Rob <git@sturob.com>
2023-07-11 03:13:04 -0400

    when moving use flock to wait for last move to finish

--- scripts/move-v.sh
+++ scripts/move-v.sh
@@ -5,6 +5,12 @@ LOG="/tmp/propane.log"
 
 dir=$1
 
+lockfile=/tmp/mylockfile
+exec 200>$lockfile
+# Wait for lock on fd 200 (associated with $lockfile) for the rest of the script
+flock -n 200 || { flock 200; }
+# lock will be automatically released when the script finishes or the file descriptor is closed
+
 declare -A tmux_dir
 tmux_dir[up]="U"
 tmux_dir[down]="D"
