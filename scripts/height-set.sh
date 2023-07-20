493cd4f1892c9d736cbe4f37a0b286d149e125de
Stu Rob <git@sturob.com>
2023-07-20 04:29:12 -0400

    just a number increases size by N, -N to decrease, =N to set

--- scripts/height-set.sh
+++ scripts/height-set.sh
@@ -4,9 +4,14 @@
 
 new_height=$1
 
-if [[ $1 =~ ^\+ || $1 =~ ^\- ]]; then
+if [[ $1 =~ ^[0-9]+$ ]]; then  # check if it's a positive integer
 	height=$(tmux display-message -p -F '#{pane_height}')
-	new_height=$((height $1))
+	new_height=$((height + $1))
+elif [[ $1 =~ ^\- ]]; then  # check if it starts with a "-"
+	height=$(tmux display-message -p -F '#{pane_height}')
+	new_height=$((height $1))  # decrease the height
+elif [[ $1 =~ ^\= ]]; then  # check if it starts with an "="
+	new_height=${1:1}  # remove the "=" sign
 fi
 
 tmux resize-pane -y $new_height
--- scripts/width-set.sh
+++ scripts/width-set.sh
@@ -4,9 +4,14 @@
 
 new_width=$1
 
-if [[ $1 =~ ^\+ || $1 =~ ^\- ]]; then
+if [[ $1 =~ ^[0-9]+$ ]]; then  # check if it's a positive integer
 	width=$(tmux display-message -p -F '#{pane_width}')
-	new_width=$((width $1))
+	new_width=$((width + $1))
+elif [[ $1 =~ ^\- ]]; then  # check if it starts with a "-"
+	width=$(tmux display-message -p -F '#{pane_width}')
+	new_width=$((width $1))  # decrease the width
+elif [[ $1 =~ ^\= ]]; then  # check if it starts with an "="
+	new_width=${1:1}  # remove the "=" sign
 fi
 
 tmux resize-pane -x $new_width
